32e1a4631a167253bea53f41691e3672624adcf6
shrinidhi.upadhyaya <shrinidhi.upadhyaya@stud.uni-bamberg.de>
2023-02-21 22:16:09 +0100

    Streamlit web app implementation change

--- app.py
+++ app.py
@@ -3,100 +3,88 @@ import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.datasets import ImageFolder
 import torchvision.transforms as tt
 import streamlit as st
 from PIL import Image
 
-print("is cuda avilable")
-print(torch.cuda.is_available())
-
+# GPU or CPU
 def get_default_device():
     """Pick GPU if available, else CPU"""
     if torch.cuda.is_available():
         return torch.device('cuda')
     else:
         return torch.device('cpu')
+
+# Move Data on the device (CPU or GPU)
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
     
-# SmallResNet9 model
-class SmallResNet9(nn.Module):
-    def __init__(self):
-        super(SmallResNet9, self).__init__()
+# ResNet9 model
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu = nn.ReLU(inplace=True)
-
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
-        )
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True)
-        )
-
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True)
-        )
+class ResNet9(nn.Module):
+    def __init__(self, num_classes=3):
+        super().__init__()
         
-        self.fc = nn.Sequential(nn.MaxPool2d(4), 
-                      nn.Flatten(), 
-                      nn.Dropout(0.2),
-                      nn.Linear(256, num_of_classes))
+        self.conv1 = conv_block(3, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out)
+        out = self.classifier(out)
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.fc(x)
-        x = F.softmax(x, dim=1)
-        return x
+        return out
     
+# Image Prediction
 def predict_image(img, model):
     # Convert to a batch of 1
-    img = img.unsqueeze(0)
-    img = img.to(device)
+    xb = to_device(img.unsqueeze(0), device)
+    model.eval()
     # Get predictions from model
-    print("##Printing shape of image",img.shape)
-    yb = model(img)
-    print(yb)
-#     # Pick index with highest probability
-    _, preds  = torch.max(yb, dim=1)
-#     # Retrieve the class label
-    return valid_ds.classes[preds[0].item()]
+    out = model(xb)
+    # Pick index with highest probability
+    prob = F.softmax(out, dim=1)[0] * 100
+    _, indices = torch.sort(out, descending=True)
+    return [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
 
 def convert_imageTo_tensor(image):
     image = image.convert('RGB')
     return transform(image)
     
+# Classes (Rock, Paper, Scissors)
+classes = ['Paper', 'Rock', 'Scissors']
 device = get_default_device()
-model = torch.load('fold0.pth')
+
+# Loading the model
+model = torch.load('./model/3Fold-25Epochs-FirstModel/fold2.pth')
 model.to(device)
 
+#Image normalization
 stats=((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-transform = tt.Compose([tt.Resize((32,32)), tt.ToTensor(), tt.Normalize(*stats)])
-valid_ds = ImageFolder('./dataset', transform)
+transform = tt.Compose([tt.Resize((32,32)),tt.ToTensor(), tt.Normalize(*stats)])
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
@@ -115,9 +103,8 @@ if file_up is not None:
     st.image(image, caption='Please wait for a few seconds...', use_column_width=True)
     st.write("")
     
-#     image = Image.open("./datasets/real-images/test/paper/nasmi_179.png")
     image_tensor = convert_imageTo_tensor(image)
-    print("Transforming image")
-    print(image_tensor.shape)
     labels = predict_image(image_tensor,model)
-    st.write("The Hand Gesture is: ",labels)
+    st.write("The Hand Gesture is: ")
+    for i in labels:
+        st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
